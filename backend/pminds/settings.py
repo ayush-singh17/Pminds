522dd045b55b8a7d36d92d960a4a722e909db61c
ayush-singh17 <ayush.shreya06@gmail.com>
2026-07-04 17:08:37 +0530

    fix celery ssl config

--- backend/pminds/settings.py
+++ backend/pminds/settings.py
@@ -131,6 +131,10 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
+if REDIS_URL.startswith('rediss://'):
+    CELERY_BROKER_USE_SSL = {'ssl_cert_reqs': 'CERT_NONE'}
+    CELERY_REDIS_BACKEND_USE_SSL = {'ssl_cert_reqs': 'CERT_NONE'}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
